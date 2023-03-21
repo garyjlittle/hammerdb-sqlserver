3013fe018b01a6e14969df3383a90d56391559ff
Gary Little <gary.j.little@gmail.com>
2023-03-21 12:14:25 -0700

    Update tpch_3_bulkload_all.sql

--- tpch_3_bulkload_all.sql
+++ tpch_3_bulkload_all.sql
@@ -1,9 +1,11 @@
 use [tpch-test];
-bulk insert customer from 'X:\TPCH SF 1 Flatfiles\customer_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert nation from 'X:\TPCH SF 1 Flatfiles\nation_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert region from 'X:\TPCH SF 1 Flatfiles\region_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert part from 'X:\TPCH SF 1 Flatfiles\part_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert supplier from 'X:\TPCH SF 1 Flatfiles\supplier_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert partsupp from 'X:\TPCH SF 1 Flatfiles\partsupp_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert orders from 'X:\TPCH SF 1 Flatfiles\orders_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert lineitem from 'X:\TPCH SF 1 Flatfiles\lineitem_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
+-- set the DB to use bulk-logging
+-- use batchsize=5000 to improve IO throughput
+bulk insert customer from 'X:\TPCH SF 1 Flatfiles\customer_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert nation from 'X:\TPCH SF 1 Flatfiles\nation_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert region from 'X:\TPCH SF 1 Flatfiles\region_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert part from 'X:\TPCH SF 1 Flatfiles\part_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert supplier from 'X:\TPCH SF 1 Flatfiles\supplier_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert partsupp from 'X:\TPCH SF 1 Flatfiles\partsupp_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert orders from 'X:\TPCH SF 1 Flatfiles\orders_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
+bulk insert lineitem from 'X:\TPCH SF 1 Flatfiles\lineitem_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|', batchsize=5000);
