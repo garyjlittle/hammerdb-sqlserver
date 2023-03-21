a8d0ba149bd7ac80dc38ad6b7330f1b691ba373d
Gary Little <gary.j.little@gmail.com>
2023-03-21 11:00:53 -0700

    Delete tpch_3_bulkload_all.sql

--- tpch_3_bulkload_all.sql
+++
@@ -1,9 +0,0 @@
-use [tpch-test];
-bulk insert customer from 'X:\TPCH SF 1 Flatfiles\customer_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert nation from 'X:\TPCH SF 1 Flatfiles\nation_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert region from 'X:\TPCH SF 1 Flatfiles\region_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert part from 'X:\TPCH SF 1 Flatfiles\part_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert supplier from 'X:\TPCH SF 1 Flatfiles\supplier_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert partsupp from 'X:\TPCH SF 1 Flatfiles\partsupp_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert orders from 'X:\TPCH SF 1 Flatfiles\orders_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
-bulk insert lineitem from 'X:\TPCH SF 1 Flatfiles\lineitem_1.tbl' with (tablock, datafiletype='char', codepage='raw', fieldterminator='|');
