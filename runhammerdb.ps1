8b637b05ed3fa423e3be795f6023368b1019ad94
Gary Little <gary.j.little@gmail.com>
2023-03-22 09:37:15 -0700

    Time hammerdb from powershell

---
+++ runhammerdb.ps1
@@ -0,0 +1,10 @@
+﻿
+Write-Host("Starting HammerDB run")
+for ($i=1 ; $i -le 4 ; $i++)
+ {
+    $startTime=(Get-Date)
+    chdir('C:\Program Files\HammerDB-4.2')
+    Start-Process 'C:\Program Files\HammerDB-4.2\hammerdbcli.bat' -ArgumentList "auto tpch_clustered_remote_run.tcl" -Wait
+    $elapsedTime=(Get-Date)-$startTime
+    Write-Host("Run", $i, $elapsedTime,$startTime)
+  }
