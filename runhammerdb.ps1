ad6e3834de7855082c2aabe19da7704a7cf5bf51
Gary Little <gary.j.little@gmail.com>
2023-03-22 12:45:57 -0700

    Update runhammerdb.ps1

--- runhammerdb.ps1
+++ runhammerdb.ps1
@@ -1,10 +1,21 @@
-﻿
-Write-Host("Starting HammerDB run")
-for ($i=1 ; $i -le 4 ; $i++)
+# Take parameter from CLI
+$HDBFileName=$args[0]
+
+if ($HDBFileName -eq $null) {
+    Write-Host "Need a HammerDB config file as first param"
+    exit
+}
+if (-not(Test-Path $HDBFileName)) {
+    Write-Host "File Not Found"
+    exit
+    }
+
+Write-Host("Starting HammerDB run for config $HDBFileName")
+for ($i=1 ; $i -le 4 ; $i++) 
  {
     $startTime=(Get-Date)
     chdir('C:\Program Files\HammerDB-4.2')
-    Start-Process 'C:\Program Files\HammerDB-4.2\hammerdbcli.bat' -ArgumentList "auto tpch_clustered_remote_run.tcl" -Wait
+    Start-Process 'C:\Program Files\HammerDB-4.2\hammerdbcli.bat' -ArgumentList "auto $HDBFileName" -Wait
     $elapsedTime=(Get-Date)-$startTime
     Write-Host("Run", $i, $elapsedTime,$startTime)
   }
