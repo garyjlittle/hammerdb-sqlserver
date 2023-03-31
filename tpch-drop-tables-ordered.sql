8171458f2bdc817a417c8d8f73a35ddb70bdc64d
Gary Little <gary.j.little@gmail.com>
2023-03-31 13:04:45 -0400

    Add files via upload

---
+++ tpch-drop-tables-ordered.sql
@@ -0,0 +1,11 @@
+use "tpch_test";
+-- Tables need to be dropped in order to 
+-- conform to constraints.
+drop table lineitem;
+drop table orders;
+drop table partsupp
+drop table supplier;
+drop table customer;
+drop table nation;
+drop table dbo.region;
+drop table dbo.part;
